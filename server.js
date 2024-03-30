4bee6bdb8240e01961fff1143c14b249a4da2d12
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2024-03-30 14:27:57 -0400

    implement cookie-based user session

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
+/*
+ *Name: Thomas van der Molen
+ *File: server.js
+ *
+ *
+ * The purpose of this js file is to function as the server's "back end" It contains the necessary server-side logic
+ * It is also where the web server connects to the database and executes queries
+ *
+ */
+
+//sources referenced during implementation
+//
+//https://www.youtube.com/watch?v=SccSCuHhOw0&t=779s 
+//https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
+//https://www.geeksforgeeks.org/http-cookies-in-node-js/
+//https://stackoverflow.com/questions/38884522/why-is-my-asynchronous-function-returning-promise-pending-instead-of-a-val
+//
+//https://medium.com/swlh/dealing-with-multiple-promises-in-javascript-41d6c21f20ff
+//https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
+//https://stackoverflow.com/questions/35737482/multiple-submit-buttons-html-express-node-js
+//
+//
 const express = require('express');
 
 const maria = require('mariadb');
 const app = express();
 
-//https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
 const bodyParser = require('body-parser');
 const multer = require('multer');
 var upload = multer();
 
-// for parsing application/json
+
 app.use(bodyParser.json()); 
 
-// for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.set("view engine", "ejs");
 
-//credit also goes to 
-//https://www.youtube.com/watch?v=SccSCuHhOw0&t=779s
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-//this is insane
-var LoggedInUser = 'ernie';;
-//utterly insane
-//the currently logged in user is tracked via a server-side variable. 
-//if the server is reset, the user will be logged out
 
 async function queryDB(query){
     let conn;
@@ -45,11 +60,10 @@ async function queryDB(query){
     }
 }
 
+//Vulnerable to client-side cookie manipulation
 app.get('/', (req, res)=>{
-
+    res.clearCookie("loginDetails");
     var users = queryDB("select * from users;");
-    //wowie this is pretty cool
-    //https://stackoverflow.com/questions/38884522/why-is-my-asynchronous-function-returning-promise-pending-instead-of-a-val
     users.then(function(result){
         res.render('index', {users:  result});
     })
@@ -57,19 +71,23 @@ app.get('/', (req, res)=>{
 });
 
 app.get("/account", (req, res)=>{
-    
+   
+
+    var LoggedInUser = req.cookies["loginDetails"]["username"];
+        
     var query = `SELECT asset, quantity FROM portfolios WHERE username='${LoggedInUser}';`;
     var assets = queryDB(query);
     query = `SELECT balance FROM users WHERE username='${LoggedInUser}';`;
     var balance = queryDB(query);
     Promise.all([assets, balance]).then(function(result){
-        //console.log(result[1]);
         res.render("account", {user: LoggedInUser, assets:result[0], balance:result[1]});
     });
    
 });
 
 app.get("/deleteAccount", (req, res)=>{
+    var LoggedInUser = req.cookies["loginDetails"]["username"];
+    res.clearCookie("loginDetails");
     var query = `DELETE FROM users WHERE username='${LoggedInUser}';`
     
     var deleteUser = queryDB(query);
@@ -91,9 +109,7 @@ app.get("/market", (req,res)=>{
     query = `select * from commodities;`
     var commodities = queryDB(query);
 
-    //https://medium.com/swlh/dealing-with-multiple-promises-in-javascript-41d6c21f20ff
     Promise.all([listings, exchanges, stocks, cryptos, commodities]).then(function(result){
-        //console.log(result[1]);
         res.render("market", {listings: result[0], exchanges: result[1],
                             stocks: result[2], cryptos: result[3],
                             commodities: result[4]
@@ -103,16 +119,9 @@ app.get("/market", (req,res)=>{
 })
 
 app.post("/login", (req, res)=>{
-    //only took me an eon to figure out what's wrong
-    //https://www.tutorialspoint.com/expressjs/expressjs_form_data.htm
-
     var inputUsername = req.body.username;
     var inputPassword = req.body.password;
     var action = req.body.loginbutton;
-
-    //this is actually amazing
-    //https://stackoverflow.com/questions/35737482/multiple-submit-buttons-html-express-node-js
-    //mind blowing, really
 
     if(inputUsername == "" || inputPassword==""){
         //invalid input
@@ -131,9 +140,6 @@ app.post("/login", (req, res)=>{
 });
 
 app.post("/trade", (req, res)=>{
-
-    //just keep this for now
-
     //the user desires to make a trade.
     //Find out if the trade was a buy or sell.
     //If it was a purchase, determine if they have enough money to make the purchase
@@ -146,6 +152,7 @@ app.post("/trade", (req, res)=>{
     var buyOrSell = req.body.buyOrSell;
     var asset = req.body.asset;
     var quantity = req.body.quantity;
+    var LoggedInUser = req.cookies["loginDetails"]["username"];
    
     //find the user's balance
     var balance = queryDB(`SELECT balance FROM users WHERE username='${LoggedInUser}';`);
@@ -171,17 +178,16 @@ app.post("/trade", (req, res)=>{
 
         if(result[2][0]){
             numberOwned = result[2][0].quantity;
-            //console.log(`number owned ${numberOwned}`);
         }
         else{
             numberOwned = 0;
         }
 
         if(buyOrSell == "buy"){
-            buyStock(asset, quantity, totalPrice, balance, numberOwned);
+            buyStock(asset, quantity, totalPrice, balance, numberOwned, LoggedInUser);
         }
         else{
-            sellStock(asset, quantity, totalPrice, balance, numberOwned);
+            sellStock(asset, quantity, totalPrice, balance, numberOwned, LoggedInUser);
         }
     });
     
@@ -190,39 +196,34 @@ app.post("/trade", (req, res)=>{
 });
 
 //helper functions for the /trade post request
-function buyStock(asset, quantity, cost, balance, numberOwned){
+function buyStock(asset, quantity, cost, balance, numberOwned, LoggedInUser){
     
-    if(cost <= balance){
+    if(cost < balance){
         //user can afford to buy
-        
-        //update their portfolio to include the stock
-        //first check if they already owned the asset
-        //if so, increase the quantity
-        //else, insert a new value into portfolios
-        //subtract from their balance
-        if(cost < balance){
-            //the user can make the purchase
             
-            var query;
-            if(numberOwned > 0){
-                //if they already owned the stock, update the value
-                query = `UPDATE portfolios SET quantity=${Number(quantity)+Number(numberOwned)} WHERE username='${LoggedInUser}' and asset='${asset}';`;
-            }
-            else{
-                //otherwise, make a new entry in portfolios
-                query = `INSERT INTO portfolios values ('${LoggedInUser}', '${asset}', ${quantity});`;
-            }
-            var updateTable = queryDB(query);
-            //update the user's balance
-            query = `UPDATE users SET balance=${Number(balance)-Number(cost)} WHERE username='${LoggedInUser}';`;
-            updateTable = queryDB(query);
-           //possibility for a race condition exists here?
+        var query;
+        if(numberOwned > 0){
+            //if they already owned the stock, update the value
+            query = `UPDATE portfolios SET quantity=${Number(quantity)+Number(numberOwned)} WHERE username='${LoggedInUser}' and asset='${asset}';`;
         }
+        else{
+           //otherwise, make a new entry in portfolios
+            query = `INSERT INTO portfolios values ('${LoggedInUser}', '${asset}', ${quantity});`;
+        }
+
+        var updatePortfolio = queryDB(query);
+        //update the user's balance
+        query = `UPDATE users SET balance=${Number(balance)-Number(cost)} WHERE username='${LoggedInUser}';`;
+        var updateBalance = queryDB(query);
+           
+	    Promise.all([updatePortfolio, updateBalance]).then(function(result){
+                return;
+	    });
         
     }
 
 }
-function sellStock(asset, quantity, cost, balance, numberOwned){
+function sellStock(asset, quantity, cost, balance, numberOwned, LoggedInUser){
     
     if(quantity <= numberOwned){
         //user has the assets to sell
@@ -232,16 +233,22 @@ function sellStock(asset, quantity, cost, balance, numberOwned){
         var query;
         query = `UPDATE portfolios SET quantity=${Number(numberOwned)-Number(quantity)} WHERE username='${LoggedInUser}' and asset='${asset}';`;
         //otherwise, make a new entry in portfolios
-        var updateTable = queryDB(query);
+        var updatePortfolio = queryDB(query);
         //update the user's balance
         query = `UPDATE users SET balance=${Number(balance)+Number(cost)} WHERE username='${LoggedInUser}';`;
-        updateTable = queryDB(query);
-
+        var updateBalance = queryDB(query);
+        var removeIfZero;
         //just do some housekeeping work - delete all portfolio entries if the quantity is zero.
         if(Number(numberOwned) - Number(quantity) == 0){
             query = `DELETE FROM portfolios WHERE quantity=0;`;
-            queryDB(query);
+            removeIfZero = queryDB(query);
         }
+        
+        Promise.all([updatePortfolio, updateBalance, removeIfZero]).then(function(result){
+        
+            return;
+	});	
+
     }
 
 }
@@ -254,8 +261,11 @@ function validateLogin(username, password, res){
     login.then(function(result){
         if (password == result[0].password){
             //login was valid
-            LoggedInUser = username;
-            res.redirect("account");
+	    //give the user a cookie containing their username, security issue
+            var login = {"username" : username}
+	    res.cookie("loginDetails", login);
+
+	    res.redirect("account");
         }
         else{
             //login was invalid
